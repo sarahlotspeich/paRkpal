868371bbf1d08c503f73365ad447293a7340f374
Sarah <sarah.c.lotspeich@vanderbilt.edu>
2021-01-07 13:21:44 -0500

    Commit package contents

---
+++ R/park_palette.R
@@ -0,0 +1,71 @@
+park.pal <- list("acadia1" = c("#544337", "#86715c", "#1f1718", "#92bef6", "#e4decf", "#b49f8e", "#648ebd", "#bbebfb", "#878187", "#3c6484"),
+                 "acadia2" = c("#544337", "#86715c", "#1f1718", "#92bef6", "#e4decf", "#b49f8e", "#648ebd", "#bbebfb", "#878187", "#3c6484"),
+                 "american_samoa" = c("#294e5a", "#fce5c8", "#677730", "#5884a1", "#4b4c14", "#d9d3c4", "#8c7f75", "#af9f99", "#3ba499", "#a3c7aa"),
+                 "antelope_canyon" = c("#fac794", "#e37c3a", "#7c1f05", "#4e0d04", "#fba139", "#c1673a", "#b54f1e", "#a1461a", "#d2898a", "#ae544b"),
+                 "arches" = c("#aa562d", "#a9d1f6", "#3c281c", "#673018", "#e77d3e", "#e79475", "#fac50f", "#8fa6cc", "#d08456", "#7e2304"),
+                 "big_bend1" = c("#fbf7b5", "#e0a975", "#232122", "#7e656b", "#4a424b", "#69535a", "#937c7a", "#9f6c69", "#443a3b", "#5a545b"),
+                 "big_bend2" = c("#423629","#fbf1ce", "#cba47d", "#816956", "#18170e", "#605550", "#9c8c7c", "#6b6457", "#696164", "#c4ac9c"),
+                 "biscayne" = c("#89cca2", "#473733", "#73975f", "#5993b0", "#4b617d", "#5c6d3e", "#846933", "#846933", "#392467", "#96be19", "#b1811a"),
+                 "bryce_canyon" = c("#db7d5a", "#5c3026", "#222325", "#715e69", "#a34934", "#a37874", "#fbd376", "#675654", "#ab9598", "#b4a498"),
+                 "canyonlands" = c("#c3937e", "#85665d", "#60a6ac", "#5e453e", "#566c6c", "#374a48", "#b0a480", "#518897", "#4a5a65", "#af3f1f"),
+                 "carlsbad_caverns" = c("#213d32", "#94c5b3", "#655633", "#4bac7c", "#bad6fa", "#9e8659", "#6a6a65", "#6e965e", "#d0c090", "#1a202c"),
+                 "cascades" = c("#7d92bb", "#484a3a", "#c0bac3", "#928771", "#5f587c", "#2e1830", "#161e19", "#a3b074", "#c4b94e", "#90c8d4"),
+                 "congaree" = c("#958b48", "#e1d4c6", "#2d2d15", "#d1d17a", "#55371e", "#5a572b", "#d1b07f", "#79542f", "#be8e50", "#b8a58b", "#447424"),
+                 "crater_lake" = c("#665379", "#a3abdf", "#da85a5", "#fe9597", "#0e1d2b", "#1e3657", "#aa6387", "#727fae", "#e94c64", "#542c4a"),
+                 "cuyahoga" = c("#423b15", "#ab832e", "#5d7864", "#2d1905", "#5b702d", "#ac540e", "#b0b2c6", "#abdeff", "#886529", "#f7c228"),
+                 "death_valley" = c("#f69396", "#cfbe22", "#5b7553", "#561831", "#232a2e", "#765422", "#4a5896", "#7c8f8f", "#a6b8b6", "#c34b77"),
+                 "denali" = c("#150000", "#653314", "#b15d27", "#93552c", "#cacec8", "#59767b", "#23292b", "#879897", "#7f7b73", "#a09f94"),
+                 "everglades" = c("#98cfe2", "#c6e3e5", "#4c5634", "#908e49", "#698a6c", "#5b9cac", "#163038", "#3d6d79", "#25231e", "#b59452"),
+                 "glacier" = c("#47819d", "#bec3d0", "#8eadca", "#171e18", "#755f6d", "#3f4a66", "#4c3f3b", "#b0a756", "#324839", "#aa45ba"),
+                 "glacier_bay" = c("#063055", "#c7ebef", "#319fe3", "#1e618f", "#7fdaed", "#7ebad3", "#285265", "#5ab8db", "#0e63b5", "#0444b4"),
+                 "grand_canyon" = c("#ffbd89", "#67424e", "#d8918b", "#9f7f9f", "#a76158", "#301d30", "#e8afe2", "#b76c30", "#b6c4fe", "#4c2718"),
+                 "grand_teton" = c("#cbb59c", "#245d94","#1984df", "#88888f", "#528776", "#354f50", "#05275a", "#5893b0", "#a45c34", "#d38f20"),
+                 "hawaii_volcanoes" = c("#dc714d", "#3e344f", "#965955", "#1b0e0c", "#fab371", "#efd692", "#726575", "#215771", "#1d2c4d", "#e26236", "#a18a9c"),
+                 "hot_springs" = c("#4e4c30", "#c9d0e2", "#7d8e49", "#a5b1ad", "#6b7372", "#3c1514", "#a8d54d", "#379c0c", "#b19c59", "#8daa85"),
+                 "isle_royale" = c("#57793e", "#6e8583", "#adc0e5", "#8b884c", "#2a141c", "#bd5b10", "#224f78", "#cdd7ed", "#8e6f4b", "#554f45"),
+                 "joshua_tree" = c("#1591a3", "#a2bec5", "#578b8e", "#95886b", "#5c4b31", "#c69754", "#2d2c20", "#786e35", "#c5ae95", "#524b44"),
+                 "katmai" = c("#d5d3d1", "#90929b", "#a4b8c9", "#141d22", "#82a3c0", "#3c7092", "#375163", "#606e70", "#013a40", "#2e4d4f"),
+                 "kenai_fjord" = c("#3f4040", "#7e9ca6", "#606363", "#d0d9dd", "#aebec5", "#abcfde", "#3f6f81", "#474b54", "#60737d", "#9accd4"),
+                 "mt_rainier1" = c("#4182cc", "#d8cfba", "#4c5a28", "#8e9365", "#847b5d", "#161e15", "#656e70", "#18301e", "#a0bd9e", "#ac743c"),
+                 "mount_rainier2" = c("#fa0a59", "#779405", "#d37bbb", "#743c84", "#5e5c71", "#6c98c3", "#364a0d", "#5d4f4b", "#948167", "#d9b3e7"),
+                 "olympic" = c("#4d5517", "#d2dac5", "#9abe4a", "#6e901e","#8f9536", "#82989f", "#111005", "#5d6760", "#291f0e", "#b79675"),
+                 "redwood" = c("#89432d", "#707235", "#336023", "#97652f", "#b84a23", "#655d5f", "#a49494", "#a5a76b", "#d9a853", "#da5d0d"),
+                 "saguaro" = c("#997a19", "#dca787", "#9e8595", "#361f14", "#ecd0b4", "#522819", "#796ba6", "#77704a", "#ba985c", "#fdd164"),
+                 "sand_dunes" = c("#cba655", "#53473a", "#7b94b3", "#a18032", "#aeb5bf", "#173455", "#73808a", "#bc865a", "#2a1908", "#bc7424"),
+                 "sequoia" = c("#d5dbd4", "#473321", "#888a74", "#9b9fa0", "#64644c", "#846147", "#b8906e", "#6a6e69", "#180e06", "#b4af6a"),
+                 "shenandoah" = c("#b0a8d1", "#dcd7df", "#925945", "#df7335", "#8d7091", "#7e190e", "#381d11", "#b09256", "#5d4233", "#e7c127"),
+                 "smoky_mountains1" = c("#8ea8c1", "#634728", "#d96c25", "#6a7b0c", "#1e2218", "#a95024", "#a19e77", "#9a7d24", "#5a150f", "#c48116"),
+                 "smoky_mountains2" = c("#263f50", "#3c5f6a", "#6f9cbb", "#d99b54", "#ffb245", "#6b5547", "#354e26", "#15242a", "#a5c1f2", "#bf9e9b"),
+                 "voyageurs" = c("#4382a3", "#feba56", "#eb8241", "#1e4f6f", "#af4b1c", "#324450", "#191c14", "#96a0a6", "#f67a12", "#d1b7aa"),
+                 "yellowstone" = c("#ea6213", "#f99625", "#ccd40a", "#7eca1f", "#0c50c7", "#37bc7e", "#2a5142", "#ae9679", "#d5dbdf", "#cedd64"),
+                 "yosemite" = c("#cad3df", "#9aaabf", "#7990ae", "#506a87", "#3e5776", "#354456", "#0f1723",  "#3e4042", "#747a82", "#b09c98"),
+                 "zion" = c("#eaf2f1", "#282722", "#977971", "#aeaba5", "#695550", "#75706c", "#4f4f47", "#9994aa", "#6e616e", "#bc7464")
+)
+
+#' A function to list available palettes.
+#'
+#' This function returns a list of the available palettes in the package.
+#' @keywords
+#' @export
+#' @examples
+#' list_park_palettes()
+
+list_park_palettes <- function(pal = park.pal) {
+  return(park.pal)
+}
+
+#' A function to return the palette based on user inputs.
+#'
+#' This function returns the palette based on the desired name and size.
+#' @param name Name of national park palette (call \code{list_park_palettes()} for available palettes).
+#' @param n Number of colors from palette. Default is the maximum of 10.
+#' @keywords
+#' @export
+#' @examples
+#' park_palette()
+
+park_palette <- function(name, n = 10) {
+  if (!(name %in% names(park.pal))) return(warning("Invalid palette name provided."))
+  return(as.vector(unlist(park.pal[name]))[1:n])
+}
+
